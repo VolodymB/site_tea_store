a4bb2d5081ec85578569bf2171cf0b306e6bd8d1
VolodymB <volodymb.com>
2021-06-15 20:17:26 +0300

    create form

--- admin/views/form.php
+++ admin/views/form.php
@@ -40,6 +40,59 @@
         </div>
       </div>
     </div>
+    <br>
+    <div class="col-sm-8">
+        <label for="unit" class="form-label">Одиниця виміру</label>
+        <div class="input-group has-validation">
+          <select name="unit" id="unit" class="form-select">
+              <option value="1">шт.</option>
+              <option value="2">0,1</option>
+              <option value="3">0,5</option>              
+          </select>
+          <div class="invalid-feedback">Оберіть одиницю виміру</div>
+        </div>
+        <div class="col-sm-4">
+        <label for="price" class="form-label">Вартість</label>
+        <div class="input-group has-validation">
+          <input
+            type="number" class="form-control" id="price" name='price' /> грн
+          <div class="invalid-feedback">Введіть ціну</div>
+          </div>
+        </div>
+    <br>
+    <p>Оберіть категорію</p>
+    <ul class="list-group">
+                            <li class="list-group-item rounded-0">
+                                <div class="custom-control custom-checkbox">
+                                    <input class="custom-control-input" name='category' id="category1" type="checkbox">
+                                    <label class="cursor-pointer font-italic d-block custom-control-label" for="customCheck1">Чай</label>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <div class="custom-control custom-checkbox">
+                                    <input class="custom-control-input" name='category' id="category2" type="checkbox">
+                                    <label class="cursor-pointer font-italic d-block custom-control-label" for="customCheck2">Пуер</label>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <div class="custom-control custom-checkbox">
+                                    <input class="custom-control-input" name='category' id="category3" type="checkbox">
+                                    <label class="cursor-pointer font-italic d-block custom-control-label" for="customCheck3">Червоний</label>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <div class="custom-control custom-checkbox">
+                                    <input class="custom-control-input" name='category' id="category4" type="checkbox">
+                                    <label class="cursor-pointer font-italic d-block custom-control-label" for="customCheck4">Зелений</label>
+                                </div>
+                            </li>
+                            <li class="list-group-item rounded-0">
+                                <div class="custom-control custom-checkbox">
+                                    <input class="custom-control-input" name='category' id="category5" type="checkbox">
+                                    <label class="cursor-pointer font-italic d-block custom-control-label" for="customCheck5">Білий</label>
+                                </div>
+                            </li>
+                        </ul>
     <hr>
     <button class="w-100 btn btn-primary btn-lg" type="submit">Зберегти</button>
   </form>
