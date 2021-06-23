305852250e0c3de873ad1be47dcf7e7b9cbb5f11
VolodymB <volodymb.com>
2021-06-23 20:49:14 +0300

    form table

--- admin/views/category.php
+++ admin/views/category.php
@@ -26,16 +26,38 @@
   <!-- form -->
 </form>
 <!-- форма кінець -->
-<table>
-<?php $i=1 ?>
-<?php foreach($data['categories'] as $category): ?>
-<tr>
-<td><?=$i++?></td>
-<td><?=$category['name']?></td>
-<td><?=$category['parent_id']?></td>
-<td><?=$category['sort_order']?></td>
-</tr>
-<?php endforeach; ?>
-
+<!-- table begin -->
+<hr>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Назва</th>
+      <th scope="col">Категорія</th>
+      <th scope="col">Сортування</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php $i=1 ?>
+    <?php foreach($data['categories'] as $category): ?>
+    <tr>
+      <th scope="row"><?=$i++?></th>
+        <td><?=$category['name']?></td>
+        <td><?=$category['parent_id']?></td>
+        <td><?=$category['sort_order']?></td>
+        <td>
+<ul class="list-inline m-0">
+                      <li class="list-inline-item">
+                      <a class="btn btn-success btn-sm rounded-0" href="/edit_category?id=<?=$category['id']?>" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-edit"></i></a>
+                      </li>
+                      <li class="list-inline-item">
+                      <a class="btn btn-danger btn-sm rounded-0" href="/delete_category?id=<?=$category['id']?>" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-trash"></i></a>
+                      </li>
+                </ul>
+</td>
+    </tr>
+    <?php endforeach; ?>
+  </tbody>
 </table>
+<!-- table end -->
 </div>
