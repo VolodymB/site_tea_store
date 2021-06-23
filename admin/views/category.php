712123193c7f70eb8f2d6e499335661a973f5c09
VolodymB <volodymb.com>
2021-06-23 20:24:26 +0300

    category form

--- admin/views/category.php
+++ admin/views/category.php
@@ -1,16 +1,29 @@
 <div class="container">
 <h1>Категорії</h1>
 <!-- Форма початок -->
+
+<!-- form -->
 <form action="" method='POST'>
-<input type="hidden" name='id'>
-<input type="text" name='name' placeholder='name'>Назва
-<select name="parent_id" >
-<?php foreach($data['categories'] as $parent_category): ?>
-<option value="<?=$parent_category['id']?>"><?=$parent_category['name']?></option>
-<?php endforeach; ?>
-</select>
-<input type="number" name='sort_order'>
-<input type="submit" name='send'>
+<input type="hidden" name='id'>   
+        <div class="mb-3">
+            <label for="" class="form-label">Назва</label>
+            <input type="text" id="name" name="name" class="form-control" placeholder="Назва">
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Категорії</label>
+            <select id="parent_name" name='parent_name' class="form-select">
+            <?php foreach($data['categories'] as $parent_category): ?>
+                <option value="<?=$parent_category['id']?>"><?=$parent_category['name']?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Назва</label>
+            <input type="number" id="sort_order" name="sort_order" class="form-control" placeholder="Сортування" min=0>
+        </div>
+    </div>
+    <button type="submit" class="btn btn-primary" name='send'>Зберегти</button>
+  <!-- form -->
 </form>
 <!-- форма кінець -->
 <table>
